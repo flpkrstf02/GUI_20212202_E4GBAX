93505d2eb3d9cce42c5dcadae8c90b2de7d2149c
Kristof Fulop <f0kristof@outlook.com>
2022-03-26 15:37:13 +0100

    Add project files.

---
+++ GUI_20212202_E4GBAX/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GUI_20212202_E4GBAX
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
