f66b6c9dfc6c4a3285e841b8da3bba52a7df18f3
Kristof Fulop <f0kristof@outlook.com>
2022-03-31 13:58:51 +0200

    Skeleton for logic, igamemodel and display

---
+++ GUI_20212202_E4GBAX/Renderer/Display.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace GUI_20212202_E4GBAX.Renderer
+{
+    public class Display : FrameworkElement
+    {
+        public void SetupLogic()
+        {
+
+        }
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            if (true)
+            {
+
+            }
+        }
+    }
+}
