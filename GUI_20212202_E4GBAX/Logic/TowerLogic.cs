5e8d2acd377beb188187d9839b906fb0f7fffe43
Zöld Anna Alma <zoldannaalma@stud.uni-obuda.hu>
2022-04-23 16:46:32 +0200

    tornyok elkésztése

---
+++ GUI_20212202_E4GBAX/Logic/TowerLogic.cs
@@ -0,0 +1,49 @@
+﻿using GUI_20212202_E4GBAX.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Text;
+using System.Threading.Tasks;
+using static GUI_20212202_E4GBAX.Logic.TowerDefenseLogic;
+
+namespace GUI_20212202_E4GBAX.Logic
+{
+    public class TowerLogic: ITowerLogic
+    {
+        private List<string> bullet;
+
+        public TowerLogic()
+        { 
+            
+        }
+
+        //alap torony, egy célpontot sebez egyszerre
+        private void Tower1Maker(Tower t)
+        {
+            t.cost = 40;
+            t.range = 2;
+            t.damage = 2;
+        }
+
+        //az alap torony fejlesztése, nagyobb sebzéssel
+        private void Tower12Maker(Tower t)
+        {
+            t.cost = 100;
+            t.range = 6;
+            t.damage = 8;
+        }
+
+        //különálló torony, területi sebzéssel
+        private void Tower2Maker(Tower t)
+        {
+            t.cost = 125;
+            t.range = 4;
+            t.damage = 10;
+        }
+
+
+
+
+    }
+}
--- GUI_20212202_E4GBAX/Models/Tower.cs
+++ GUI_20212202_E4GBAX/Models/Tower.cs
@@ -1,6 +1,20 @@
-﻿namespace GUI_20212202_E4GBAX.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace GUI_20212202_E4GBAX.Models
 {
     public class Tower
     {
+        public int range {get; set;}
+        public int damage { get; set; }
+
+        public int cost { get; set; }
+
+       
+
     }
 }
